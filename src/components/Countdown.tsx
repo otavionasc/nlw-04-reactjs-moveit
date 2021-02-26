b677ef8798e394adbf30836860df7b6b602c1e9c
Otávio Nascimento <ohn.94@hotmail.com>
2021-02-25 21:15:13 -0300

    Desafio 04

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,46 +1,20 @@
-import { useState, useEffect, useContext } from "react";
-import { ChallengesContext } from "../contexts/ChallengesContext";
+import { useContext } from "react";
+import { CountdownContext } from "../contexts/CountdownContext";
 import styles from "../styles/components/Countdown.module.css";
 
-let countdownTimeout: NodeJS.Timeout;
-
 export function Countdown() {
-  const { startNewChallenge } = useContext(ChallengesContext);
-
-  const [time, setTime] = useState(25 * 60);
-  const [isActive, setIsActive] = useState(false);
-  const [hasFinished, setHasFinished] = useState(false);
-
-  const minutes = Math.floor(time / 60);
-  const seconds = time % 60;
+  const {
+    minutes,
+    seconds,
+    hasFinished,
+    isActive,
+    startCountdown,
+    resetCountdown
+  } = useContext(CountdownContext);
 
   // padStart = verifica se a string tem 2 caracteres, se não tiver, coloca o zero na frente
   const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
-
-  function startCountdown() {
-    setIsActive(true);
-  }
-
-  function resetCountdown() {
-    // clearTimeout = Faz com que o countdown não execute por conta do delay de 1seg. Para na hora o contador.
-    clearTimeout(countdownTimeout);
-    setIsActive(false);
-    setTime(25 * 60);
-  }
-
-  // [] Array de dependencias do use effect = toda vez que a variável isActive ou time mudar, ele executará uma função
-  useEffect(() => {
-    if (isActive && time > 0) {
-      countdownTimeout = setTimeout(() => {
-        setTime(time - 1);
-      }, 1000)
-    } else if (isActive && time === 0) {
-      setHasFinished(true);
-      setIsActive(false);
-      startNewChallenge();
-    }
-  }, [isActive, time]);
 
   return (
     <div>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/otavionasc.png" alt="Otávio Nascimento" />
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Otávio Nascimento</strong>
         <p>
           <img src="icons/level.svg" alt="level" />
-          level 1
+          level {level}
         </p>
       </div>
     </div>
